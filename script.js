e5028a9825626cb883a40eda0f715d3283ce5a47
Alex Hu <xindi_hu@yahoo.com>
2020-12-13 13:20:09 -0600

    first version

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const videoElement = document.getElementById('vedio');
+const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-
+// Prompt to select media stream,pass to vedio element,then play
 async function selectMediaStream(){
     try {
-
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     }catch(error){
-
+        // Catch error here
+        console.log('selectMediaStrem error',  error)
     }
 }
+
+button.addEventListener('click',async () => {
+    //Disable button
+    button.disabled = true;
+    //Start p in p
+    await videoElement.requestPictureInPicture();
+    //Reset button
+    button.disabled = false;
+});
 
 
 
